c4d972aa8832bfb1a21e46757300f98f7f0b82a7
Aman <aman.d6t@gmail.com>
2024-06-21 22:59:15 +0530

    commit

--- src/components/custom-tabs/tab-test.jsx
+++ src/components/custom-tabs/tab-test.jsx
@@ -5,21 +5,6 @@ function RandomComponent() {
 }
 
 export default function TabTest() {
-  const tabs = [
-    {
-      label: "Tab1",
-      content: "This is the content for Tab1",
-    },
-    {
-      label: "Tab2",
-      content: "This is the content for Tab2",
-    },
-    {
-      label: "Tab3",
-      content: <RandomComponent />,
-    },
-  ];
-
   return (
     <div>
       <Tabs tabsContent={tabs} />
